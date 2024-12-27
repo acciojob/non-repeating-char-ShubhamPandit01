abe5af7abb4948fb305a1deed1afcc8347a6d4ad
ShubhamPandit01 <Shubhampandit75572@gmail.com>
2024-12-27 15:27:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let arr = str.split('')
+	let ans = ''
+	for(let i=0; i<arr.length; i++){
+		let count = 0
+		for(let j=i+1; j<arr.length; j++){
+			if(arr[i] != arr[j]){
+				ans = arr[i]
+			}
+		}
+	}
+	return ans
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
