d09b554d94f21baf00d492747fd66c10524ebe09
ShubhamPandit01 <Shubhampandit75572@gmail.com>
2024-12-31 15:05:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function firstNonRepeatedChar(str) {
  // Write your code here
 	
 	let map = new Map()
+	let flag = false
 	for(let t of str){
 		map.has(t)? map.set(t,map.get(t)+1): map.set(t,1)
 	}
@@ -10,14 +11,15 @@ function firstNonRepeatedChar(str) {
 		if(t[1] == 1){
 			if(t[0]<ans){
 				ans = t[0]
+				flag = true
 			}
 		}
-		else{
-			return null
-		}
+	
+	}
+	if(flag == false){
+		return null
 	}
 	return ans
 }
-
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
