c5221aca3265b2d7e33433071cac2c1a662f052c
ShubhamPandit01 <Shubhampandit75572@gmail.com>
2024-12-31 14:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-	let arr = str.split('')
-	let ans = ''
-	for(let i=0; i<arr.length; i++){
-		let count = 0
-		for(let j=i+1; j<arr.length; j++){
-			if(arr[i] != arr[j]){
-				ans = arr[i]
+	
+	let map = new Map()
+	for(let t of str){
+		map.has(t)? map.set(t,map.get(t)+1): map.set(t,1)
+	}
+	let ans = 'z'
+	for(let t of map){
+		if(t[1] == 1){
+			if(t[0]<ans){
+				ans = t[0]
 			}
+		}
+		else{
+			return null
 		}
 	}
 	return ans
-	
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
